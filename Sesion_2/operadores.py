b1bdac4bad660c28b127cd0adb8160e4f4a1eb00
emanueljmendoza-a11y <emanueljmendoza@uamv.edu.ni>
2026-08-19 07:20:41 -0700

    first commit

---
+++ Sesion_2/operadores.py
@@ -0,0 +1,17 @@
+#Leer dos numeros y mostrar la suma
+#Operadores aritmeticos 
+
+numero1 = int(input ("Ingresa otro #"))
+numero2 = int(input ("Ingresa otro #"))
+
+suma= numero1 + numero2
+resta = numero1 - numero2
+multiplacion= numero1 * numero2
+division - numero1 / numero2
+divisible = numero1 % numero2
+
+print(f"suma: " {suma}")
+print(f"suma: " {resta}")
+print(f"suma: " {multiplacion}")
+print(f"suma: " {division}")
+print(f"suma: " {divisible}")
